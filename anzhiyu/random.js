1ff9ea2648dd0f729d315a606847d47afcf471bf
eh <3137536162@qq.com>
2024-07-23 17:08:10 +0800

    Site updated: 2024-07-23 17:08:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/21/hello-world/"];function toRandomPost(){
+var posts=["2024/07/21/hello-world/","2024/07/23/esp32、esp8266接入小爱同学/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
